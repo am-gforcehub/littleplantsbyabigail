779bc6e1b7df71b15946c1e18e192bb997426f85
am-gfource <aliciagarcia61@gmail.com>
2022-04-04 18:05:35 -0500

    added carousel and pics

--- app.js
+++ app.js
@@ -8,14 +8,3 @@ myDropdown.addEventListener('show.bs.dropdown', function () {
 //...do something
 })
 
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-})
-
-var myCarousel = document.getElementById('myCarousel')
-
-myCarousel.addEventListener('slide.bs.carousel', function () {
-  // do something...
-})
